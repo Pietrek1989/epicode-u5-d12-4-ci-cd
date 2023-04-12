d2612a3a90428abec74bcccf7fd491a4935af942
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-04-12 16:01:31 +0200

    testing solutions

--- src/server.js
+++ src/server.js
@@ -1,23 +1,32 @@
-import Express from "express"
-import cors from "cors"
-import productsRouter from "./api/products/index.js"
-import { badRequestHandler, genericErrorHandler } from "./errorHandlers.js"
+import Express from "express";
+import cors from "cors";
+import productsRouter from "./api/products/index.js";
+import {
+  badRequestHandler,
+  forbiddenErrorHandler,
+  genericErrorHandler,
+  notfoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
 
-const server = Express()
+const server = Express();
 
 // ************************************* MIDDLEWARES **********************************
-server.use(cors())
-server.use(Express.json())
+server.use(cors());
+server.use(Express.json());
 
 // ************************************** ENDPOINTS ***********************************
-server.use("/products", productsRouter)
+server.use("/products", productsRouter);
 
 // server.get("/test", (req, res, next) => {
 //   res.status(201).send({ message: "TEST SUCCESSFULL" })
 // })
 
 // ************************************* ERROR HANDLERS *******************************
-server.use(badRequestHandler)
-server.use(genericErrorHandler)
 
-export default server
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notfoundHandler);
+server.use(genericErrorHandler);
+server.use(forbiddenErrorHandler);
+export default server;
